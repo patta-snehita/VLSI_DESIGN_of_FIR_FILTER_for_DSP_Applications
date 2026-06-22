575dbe3e06cb6bd3c219c4b38bd77d1767c1267e
Snehita Patta <snehitapatta624@gmail.com>
2026-06-22 22:51:27 +0530

    Create testbench.v

---
+++ 16 Tap pipelined+CSA+Symmetric FIR FILTER/testbench.v
@@ -0,0 +1,64 @@
+`timescale 1ns / 1ps
+////////////////////////////////////////////////////////////////////////////////
+
+module FIR_FILTER_16tap_tb(
+    );
+    reg clk,rst;
+    reg signed [7:0]x_in;
+    wire signed [31:0]y_out;
+   // FIR_FILTER dut(clk,rst,x_in,y_out);
+   FIR_FILTER_16TAP dut(clk,rst,x_in,y_out);
+    always #5 clk=~clk;
+    initial
+    begin
+    $monitor("time=%0t| input=%d| output=%d",$time,x_in,y_out);
+     clk=0;
+     rst=1;
+     x_in=0;
+     #20;
+     rst=0;
+     x_in=2;
+     #10
+     x_in=4;
+     #10
+     x_in=6;
+     #10
+     x_in=8;
+     #10
+     x_in=10;
+     #10
+     x_in=12;
+     #10
+     x_in=14;
+     #10
+     x_in=16;
+     #10
+     x_in=18;
+     #10;
+     x_in=20;
+     #10;
+     x_in=2;
+     #10;
+     x_in=4;
+     #10;
+     x_in=6;
+     #10;
+     x_in=8;
+     #10;
+     x_in=10;
+     #10;
+     x_in=4;
+     #10;
+     x_in=0;
+     #10;
+     x_in=0;
+     #10;
+     x_in=0;
+     #10;
+     x_in=0;
+     #10;
+     #100;
+     $finish;
+  end  
+endmodule
+
